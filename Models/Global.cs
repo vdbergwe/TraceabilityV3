69f3182a4470ba33c92ec3f0dfa4ef3318ff68d5
Werner van der Berg <werner.vanderberg@rclfoods.com>
2025-03-26 10:31:31 +0200

    Created Global Variable for simplified URL updates

---
+++ Models/Global.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TraceabilityV3.Models
+{
+    public static class Global
+    {
+        public static string CentralURL { get; set; } = "https://rclmldev01.tsb.co.za:3448/";
+    }
+}
