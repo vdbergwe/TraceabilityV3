cf41e0e84c7c16b9426541abf3bc184caf716875
Werner van der Berg <werner.vanderberg@rclfoods.com>
2025-04-04 10:23:49 +0200

    Update Global urls

--- Models/Global.cs
+++ Models/Global.cs
@@ -7,7 +7,9 @@ namespace TraceabilityV3.Models
 {
     public static class Global
     {
-        //public static string CentralURL { get; set; } = "https://rclmldev01.tsb.co.za:3448/";
-        public static string CentralURL { get; set; } = "https://localhost:44314/";
+        //public static string CentralURL { get; set; } = "https://rclmldev01.tsb.co.za:3448/";     //DEV HOSTED
+        //public static string CentralURL { get; set; } = "https://localhost:44314/";               //DEV LOCAL
+        public static string CentralURL { get; set; } = "https://rclstrace01.tsb.co.za:4443/";      //LIVE CENTRAL
+
     }
 }
